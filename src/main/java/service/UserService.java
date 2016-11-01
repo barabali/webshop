c6220b9fa31fc8921349a338b3aa2094419e8fdd
evavegh <eva.vegh.93@gmail.com>
2016-11-01 16:36:27 +0100

    New exception created and used in UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exception.RegistrationFailedException;
 import model.order.Order;
 import model.order.OrderStatus;
 import model.user.Role;
@@ -13,41 +14,43 @@ public class UserService {
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
-	
-	public void register(String email, String password, String name, String address) {
-		if(userRepository.findByEmail(email) != null) {
-			return;
+
+	public void register(String email, String password, String name, String address)
+			throws RegistrationFailedException {
+		if (email == null || password == null || name == null || address == null
+				|| userRepository.findByEmail(email) != null) {
+			throw new RegistrationFailedException();
 		}
 		User user = new User(name, email, address, password);
 		user.addRole(Role.REGISTERED);
 		userRepository.create(user);
 	}
-	
+
 	public User login(String email, String password) {
 		User user = userRepository.findByEmail(email);
-		if(user == null) {
+		if (user == null) {
 			return null;
 		}
-		if(!user.getPassword().equals(password)) {
+		if (!user.getPassword().equals(password)) {
 			return null;
 		}
 		return user;
 	}
-	
-	public void changePassword(String email, String newpassword){
+
+	public void changePassword(String email, String newpassword) {
 		User user = userRepository.findByEmail(email);
-		if(user == null) {
+		if (user == null) {
 			return;
 		}
 		user.setPassword(newpassword);
 		userRepository.update(user);
 	}
 
-	public void setOrderStatus(String email, long orderId,OrderStatus status){
+	public void setOrderStatus(String email, long orderId, OrderStatus status) {
 		User user = userRepository.findByEmail(email);
 		Order order = user.findById(orderId);
 		order.setOrder(status);
 		userRepository.update(user);
 	}
-	
+
 }
