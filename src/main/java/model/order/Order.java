c67ce92520b24914fe5763e4af6d26b93a49538d
bszeman9 <bszeman9@gmail.com>
2016-11-14 16:59:35 +0100

    Order, User classes annoted

--- src/main/java/model/order/Order.java
+++ src/main/java/model/order/Order.java
@@ -3,17 +3,37 @@ package model.order;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import model.CombinedOrderItem;
 import model.OrderItem;
 import model.user.User;
 
+@Entity
+@Table(name = "order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@ManyToOne
 	private User user;
+	@OneToMany 
 	private Map<OrderItem, Integer> orderItems;
+	@OneToMany
 	private Map<CombinedOrderItem, Integer> combinedOrderItems;
+	@Enumerated(EnumType.STRING)
 	private OrderStatus order;
+	@Column(name = "total_price", precision = 7, scale = 2)
 	private BigDecimal totalPrice;
 	
 	public Order(User user, Map<OrderItem, Integer> orderItems, Map<CombinedOrderItem, Integer> combineditems) {
--- src/main/java/model/user/User.java
+++ src/main/java/model/user/User.java
@@ -1,23 +1,39 @@
 package model.user;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import model.order.Order;
 
+@Entity
+@Table(name = "user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "address")
 	private String address;
+	@Column(name = "password")
 	private String password;
-
+	@ManyToMany
 	private Set<Role> roles = new HashSet<>();
+	@OneToMany
 	private List<Order> orders = new ArrayList<>();
 
 	public User(String name, String email, String address, String password) {
